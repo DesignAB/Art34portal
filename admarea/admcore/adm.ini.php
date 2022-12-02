cb5d31cf5164580e3662930ed7b152a67ee6e47a
DesignAB <daandbdew@gmail.com>
2022-12-02 14:32:11 +0100

    Core App Modifications

---
+++ admarea/admcore/adm.ini.php
@@ -0,0 +1,19 @@
+<?php
+//admin ini
+// require "../app/core/config.php";
+// require "../app/core/website-options.php";
+// require "../app/core/controller.php";
+// require "../app/core/app.php";
+// require "../app/core/database.php";
+// require "../app/core/queries.php";
+// require "../app/core/functions.php";
+
+
+
+require "../app/core/config.php";
+require "../app/core/app.php";
+require "../app/core/database.php";
+require "../app/core/queries.php";
+require "../app/core/functions.php";
+require "../app/core/website-options.php";
+require "../app/core/controller.php";
