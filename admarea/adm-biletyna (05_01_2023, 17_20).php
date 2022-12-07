0f5f86ae62d75daaffc797fb9af2f82aad9f00b4
DesignAB <daandbdew@gmail.com>
2022-12-07 09:22:46 +0100

    Biletyna events module

---
+++ admarea/adm-biletyna (05_01_2023, 17_20).php
@@ -0,0 +1,164 @@
+<?php
+  $messages = $this->loadAdmModel("admmessenger");
+  $data['messenger'] = $messages;
+  $timecontroller = $data['timecontroller'];
+?>
+<!DOCTYPE html>
+
+<html lang="pl">
+  <head>
+    <!-- Bootstrap CSS -->
+<?php
+// $load_me->loadFileFromIncludes('_includes/all-meta.php');
+
+?>
+
+<?php
+include ('adm_includes/all_meta.php');
+include ('assets/css/all-css.php');
+?>
+    <title><?=$data['page_title']?></title>
+  </head>
+  <body>
+<?php
+$this->admInclude("/adm_views/admnavi.view.php", $data);
+
+?>
+    <div class="container-lg px-4" style="min-height: 100vh;">
+      <div class="row align-items-center justify-content-center" style="min-height: 100vh;">
+        <div class="col-12 mb-3 shadow py-md-5 " >
+          <div class="row g-0">
+        <?php $adm_message = $data['messenger']->admMessage();?>
+
+
+        <?php if(empty(API_ID) OR empty(API_URL) OR empty(API_KEY)):?>
+        <div class="col-12 shadow bg-danger p-3 mb-3 text-center">
+          <h5 class="text-white">Błąd konfigiracji integracyjnej z biletyna.pl</h5>
+        <?php if(empty(API_ID)):?>
+          <h6 class="text-white">Brak API_ID</h6>
+        <?php endif;?>
+        <?php if(empty(API_URL)):?>
+          <h6 class="text-white">Brak API_URL</h6>
+        <?php endif;?>
+        <?php if(empty(API_KEY)):?>
+          <h6 class="text-white">Brak API_KEY</h6>
+        <?php endif; //empty(biletyna_id) OR empty(API_URL) OR empty(API_KEY))?>
+
+
+        </div>
+        <?php endif;?>
+
+        <?php if(!empty(API_ID) AND !empty(API_URL) AND !empty(API_KEY)):?>
+
+              <div class="col-12 col-md-4 px-3">
+                <div class="border border-light py-3 shadow-sm text-center">
+                  <form method="POST" enctype="multipart/form-data" class="row g-3 custom-form"  id="register-form" >
+                  <input type="hidden" name="insert_new" value="insert_new">
+                    <div class="col-12 text-center">
+                      <button id="register-button" class="btn btn-sm btn-danger shadow-sm">Pobierz nowe z biletyna.pl</button>
+                    </div>
+                  </form>
+                </div>
+              </div><!-- .col-4-->
+
+              <div class="col-12 col-md-4 mb-3 px-3">
+                <div class="border border-light py-3 shadow-sm text-center">
+                  <form method="POST" enctype="multipart/form-data" class="row g-3 custom-form"  id="register-form" >
+                  <input type="hidden" name="update_records" value="update_records">
+                    <div class="col-12 text-center">
+                      <button id="register-button" class="btn btn-sm btn-danger shadow-sm">Uaktualnij wydarzenia</button>
+                    </div>
+                  </form>
+                </div>
+
+              </div><!-- .col-4-->
+              <?php if (isset($_POST['inactive_only'])):?>
+              <div class="col-12 col-md-4 mb-3 px-3">
+                <div class="border border-light py-3 shadow-sm text-center">
+                  <form method="POST" enctype="multipart/form-data" class="row g-3 custom-form"  id="register-form" >
+                  <input type="hidden" name="inactive_only" value="inactive_only">
+                    <div class="col-12 text-center">
+                      <a href="adm_events" id="register-button" class="btn btn-sm btn-success shadow-sm">Pokaż aktualne</a>
+                    </div>
+                  </form>
+                </div>
+              </div><!-- .col-4-->
+
+              <?php endif; //isset($_POST['inactive_only'])?>
+              <?php if (!isset($_POST['inactive_only'])):?>
+              <div class="col-md-4 mb-3 px-3">
+                <div class="border border-light py-3 shadow-sm text-center">
+                  <form method="POST" enctype="multipart/form-data" class="row g-3 custom-form"  id="register-form" >
+                  <input type="hidden" name="inactive_only" value="inactive_only">
+                    <div class="col-12 text-center">
+                      <button id="register-button" class="btn btn-sm btn-secondary shadow-sm">Pokaż archiwalne</button>
+                    </div>
+                  </form>
+                </div>
+              </div><!-- .col-4-->
+
+              <?php endif; //!isset($_POST['inactive_only']?>
+
+        <?php endif; //!empty(biletyna_id) AND !empty(biletyna_url) AND !empty(biletyna_key)?>
+
+
+        
+          <?php if (!empty($data['biletyna_row'])):?>
+          <?php 
+          $counter = 0;
+          foreach ($data['biletyna_row'] as $key => $value):
+          ?>
+              <!-- content cols-->
+
+              <div class="col-12 border border-light mb-3 shadow-sm">
+                <div class="row p-3">
+
+
+                  <div class="col-md-3 headings d-flex flex-column align-items-start justify-content-md-center">
+                    <h6><?=$value['artist_name'];?></h5>
+                    <h6><?=$value['event_date'];?></h5>
+                    <p class="small"><?=$value['hit_counter'];?></p>
+                  </div>
+
+                  <div class="col-6 col-md-2 d-flex flex-row align-items-center">
+                    <p><i class="bi bi-sunrise lead text-success"> </i><?=$value['start_date'];?></p>
+                  </div>
+                  <div class="col-6 col-md-2 d-flex flex-row align-items-center">
+                    <p class="text-muted"><i class="bi bi-sunset lead text-secondary"> </i><?=$value['end_date'];?></p>
+                  </div>
+                  <div class="col-md-5 d-flex flex-row align-items-center justify-content-md-end">
+                  <span class="badge <?=$timecontroller[$counter]['time_color'];?> rounded-pill p-2 me-1"><?=$timecontroller[$counter]['time_message'];?></span>
+
+                    <a href="/adm_event/<?=$value['event_id'];?>" class="btn btn-sm btn-outline-success me-1">edytuj <?=$value['event_id'];?></a>
+
+
+
+                  </div>
+
+
+
+                </div>
+              </div>
+              <!-- content cols-->
+
+
+          <?php $counter++; endforeach;?>
+          <?php endif;?>
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+<?php
+$this->admInclude("/adm_views/adminfooter.view.php", $data);
+?>
+
+<?php
+    include ('assets/js/essential-js.php');
+
+?>
+
+  </body>
+</html>
---
+++ admarea/adm-biletyna-event.php
@@ -0,0 +1,307 @@
+<?php
+  $messages = $this->loadAdmModel("admmessenger");
+  $data['messenger'] = $messages;
+  $biletyna_event = $data['biletyna_event'];
+
+?>
+<!DOCTYPE html>
+
+<html lang="pl">
+  <head>
+    <!-- Bootstrap CSS -->
+<?php
+// $load_me->loadFileFromIncludes('_includes/all-meta.php');
+
+?>
+
+<?php
+include ('adm_includes/all_meta.php');
+include ('assets/css/all-css.php');
+?>
+
+<link rel="stylesheet" href="admarea/assets/css/croppie.css">
+<link rel="stylesheet" href="admarea/assets/css/trumbowyg.css">
+
+    <title><?=$data['page_title']?></title>
+  </head>
+  <body>
+<?php
+$this->admInclude("/adm_views/admnavi.view.php", $data);
+
+?>
+    <div class="container-lg px-4" style="min-height: 100vh;">
+      <div class="row align-items-center justify-content-center" style="min-height: 100vh;">
+                  <div class="col-12 text-center mb-3">
+                    <a href="adm_events" class="btn btn-sm btn-success content-button">wróć!</a>
+                  </div>
+
+        <div class="col-12 mb-3 shadow py-md-5 " >
+          <div class="row g-0">
+        <?php $adm_message = $data['messenger']->admMessage();?>
+          <!-- heading-->
+          <div class="col-12 border border-success py-3 px-3 mb-2">
+
+
+            <h5><?=$biletyna_event['artist_name'];?></h5>
+            <h6><?=$biletyna_event['event_date'];?></h6>
+            <p>minimalne wymiary obrazka: wysokość: <?=$data['image_height'];?>px | szerokość <?=$data['image_width'];?>px</p>
+            <p class="text-danger">mniejszych obrazków proszę nie powiększać</p>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  usuń
+</button>
+
+<!-- delete modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title fs-5" id="exampleModalLabel">Modal title</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+
+      <div class="modal-body">
+            <form method="POST" enctype="multipart/form-data" class="custom-form"  id="delete-form" >
+                <input type="hidden" class="form-control" value="<?=$biletyna_event['event_id'];?>" name="event_id">
+                <input type="hidden" class="form-control" value="delete_me" name="delete_me">
+      </div>
+      <div class="modal-footer">
+        <button id="delete-button" class="btn btn-sm btn-danger register-button">usuń</button>
+        <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal">jednak nie</button>
+      </div>
+            </form>
+
+    </div>
+  </div>
+</div>
+<!-- delete modal -->
+
+
+          </div>
+          <!-- heading-->
+
+
+
+
+          <!-- options form -->
+          <div class="col-12 border border-danger py-3 px-3">
+          <h5 class="ps-1 mb-3">Opcje</h5>
+            <form method="POST" enctype="multipart/form-data" class="row g-2 custom-form"  id="register-form" >
+                <input type="hidden" class="form-control" value="<?=$biletyna_event['event_id'];?>" name="event_id">
+                <input type="hidden" class="form-control" value="<?=$biletyna_event['custom_event_image'];?>" name="custom_event_image">
+                <input type="hidden" class="form-control" name="options_update" value="options_update">
+                <!--
+                  <div class="col-md-2 d-flex align-items-end justify-content-start">
+                  <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="active" name="active" <?=$data['biletyna_event_active'];?>>
+                    <label class="form-check-label" for="active">Aktywny</label>
+                  </div>
+                  </div>
+                -->
+                  <div class="col-md-2 d-flex align-items-end justify-content-start">
+                  <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="has_image" name="has_image" <?=$data['biletyna_event_image'];?>>
+                    <label class="form-check-label" for="has_image">Własne zdjęcie</label>
+                  </div>
+                  </div>
+                  
+                  <div class="col-md-2 d-flex align-items-end justify-content-start d-none">
+                  <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="featured" name="featured" <?=$data['biletyna_event_featured'];?>>
+                    <label class="form-check-label" for="featured">Wyróżniony</label>
+                  </div>
+                  </div>
+                  
+                  
+
+
+
+
+
+                  <div class="col-12 separator">
+                    
+                  </div>
+
+
+
+                  <div class="col-12 d-flex align-items-end justify-content-end">
+                    <button id="register-button" class="btn btn-sm btn-danger register-button">zmień opcje</button>
+                  </div>
+
+
+            </form>
+          </div>
+          <!-- options form -->
+          <!-- content form -->
+          <div class="col-12 border py-3 px-3 mt-4">
+          <h5 class="ps-1 mb-3">Treść</h5>
+
+                <form method="POST" enctype="multipart/form-data" class="row g-3 custom-form"  id="register-form" >
+                  <input type="hidden" id="id" class="form-control form-control-sm" name="event_id" value="<?=$biletyna_event['event_id'];?>">
+                  <input type="hidden" id="id" class="form-control form-control-sm" name="update_content" value="update_content">
+
+
+                  <!-- name and surname -->
+                  <!-- headers -->
+                  <div class="col-md-6">
+                    <label for="contest_name" class="form-label w-100 ps-1">Nagłówek
+                    </label>
+                    <textarea type="text" name="artist_name" class="form-control ps-1 bg-white" aria-label="With textarea" rows="1" disabled><?=$biletyna_event['artist_name'];?></textarea>
+                  </div>
+                  <div class="col-md-6">
+                    <label for="contest_sub_name" class="form-label w-100 ps-1">2 Nagłówek
+                    </label>
+                    <textarea type="text" name="subheader" class="form-control ps-1 " aria-label="With textarea" rows="1" ><?=$biletyna_event['subheader'];?></textarea>
+                  </div>
+                  <!-- headers -->
+
+                  <div class="col-md-6 d-none">
+                    <label for="short_content" class="form-label w-100 ps-1">Opis skrócony
+                    </label>
+                    <textarea type="text" name="short_content" class="form-control ps-1 trumbowyg-simple" aria-label="With textarea"><?=$biletyna_event['short_content'];?></textarea>
+                  </div>
+                  <div class="col-md-12">
+                    <label for="content" class="form-label w-100 ps-1">Opis
+                    </label>
+                    <textarea type="text" name="content" class="form-control ps-1 trumbowyg-with-list" aria-label="With textarea" disabled><?=$biletyna_event['artist_description'];?></textarea>
+                  </div>
+
+
+
+
+                  <div class="col-12 text-md-end text-center ">
+                    <button id="content-button" class="btn btn-sm btn-success content-button">zmień treść</button>
+                  </div>
+
+                </form>
+          </div><!-- col 12-->
+          <!-- content form -->
+
+
+
+          <!-- image upload form-->
+          <?php if ($biletyna_event['has_image'] == 'on'):
+
+          $data['old_image'] = $biletyna_event['custom_event_image'];
+          $data['prefix'] = 'custom_event_image';
+          $data['form_title'] = 'Własne zdjęcie';
+          $data['table'] = 'events';
+          $this->admInclude("/adm_views/adm-upload-form.php", $data);
+
+          ?>
+          <!-- content form -->
+          <div class="col-12 border py-3 px-3 mt-4 d-none">
+          <h5 class="ps-1 mb-3">Alt images SEO</h5>
+
+                <form method="POST" enctype="multipart/form-data" class="row g-3 custom-form"  id="register-form" >
+                  <input type="hidden" id="id" class="form-control form-control-sm" name="id" value="<?=$biletyna_event['id'];?>">
+                  <input type="hidden" id="id" class="form-control form-control-sm" name="update_seo" value="update_seo">
+
+
+                  <!-- name and surname -->
+                  <!-- headers -->
+                  <div class="col-md-6">
+                    <label for="contest_name" class="form-label w-100 ps-1">image_alt
+                    </label>
+                    <textarea type="text" name="image_alt" class="form-control ps-1" aria-label="With textarea" rows="1" required><?=$biletyna_event['image_alt'];?></textarea>
+                  </div>
+                  <div class="col-md-6">
+                    <label for="image_mobile_alt" class="form-label w-100 ps-1">image_mobile_alt
+                    </label>
+                    <textarea type="text" name="image_mobile_alt" class="form-control ps-1" aria-label="With textarea" rows="1" ><?=$biletyna_event['image_mobile_alt'];?></textarea>
+                  </div>
+                  <div class="col-md-6">
+                    <label for="image_index_alt" class="form-label w-100 ps-1">image_index_alt
+                    </label>
+                    <textarea type="text" name="image_index_alt" class="form-control ps-1" aria-label="With textarea" rows="1" ><?=$biletyna_event['image_index_alt'];?></textarea>
+                  </div>
+                  <div class="col-md-6">
+                    <label for="index_mobile_image_alt" class="form-label w-100 ps-1">index_mobile_image_alt
+                    </label>
+                    <textarea type="text" name="index_mobile_image_alt" class="form-control ps-1" aria-label="With textarea" rows="1" ><?=$biletyna_event['index_mobile_image_alt'];?></textarea>
+                  </div>
+                  <!-- headers -->
+
+                  <div class="col-12 text-md-end text-center ">
+                    <button id="register-button" class="btn btn-sm btn-success register-button">zmień</button>
+                  </div>
+
+                </form>
+          </div><!-- col 12-->
+          <!-- content form -->
+
+
+          <?php endif;?>
+          <!-- image upload form-->
+
+
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+<?php
+$this->admInclude("/adm_views/adminfooter.view.php", $navbar_categories);
+?>
+
+<?php
+    include ('assets/js/essential-js.php');
+// admInclude("/assets/js/trumbowyg.js", $data);
+?>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+    window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')
+    </script>
+  <script src="admarea/assets/js/croppie.js"></script>
+
+<script src="admarea/assets/js/trumbowyg.js"></script>
+
+<script>
+
+$.trumbowyg.svgPath = 'admarea/assets/css/icons.svg'; 
+
+$('.trumbowyg-with-list').trumbowyg({
+  
+btns: [['bold'], ['h4'], ['h5'], ['p'], ['unorderedList'], ['removeformat'], ['viewHTML'], ['link']],
+tagsToRemove: ['table', 'td', 'tr']
+
+
+}); 
+$('.trumbowyg-simple').trumbowyg({
+  
+btns: [['bold'], ['p'], ['removeformat'], ['viewHTML']],
+tagsToRemove: ['table', 'td', 'tr']
+
+}); 
+
+
+
+</script>
+<?php
+if ($biletyna_event['has_image'] == 'on') {
+  // main image
+  // $data['image_height'] = 500;
+  // $data['image_width'] = 500;
+  $data['prefix'] = 'custom_event_image';
+  $data['image_q'] = '.9';
+  $this->admInclude("/assets/admin-croopie-model-script.php", $data);
+  // main image
+
+
+
+
+
+
+
+  }
+      
+
+
+
+?>
+
+
+  </body>
+</html>
